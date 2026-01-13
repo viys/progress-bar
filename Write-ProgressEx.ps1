4fe8f937179b830918d96cefa8ecc8992e8f2349
Jiyon <viysyu@gmail.com>
2026-01-13 23:10:27 +0800

    将用例注释掉

--- Write-ProgressEx.ps1
+++ Write-ProgressEx.ps1
@@ -1,18 +1,22 @@
 Install-Module -Name Write-ProgressEx
 
-# $total = 100
+# ===== Demo =====
+<#
+$total = 100
 
-# for ($i = 1; $i -le $total; $i++) {
-#     Write-ProgressEx `
-#         -Activity "写入 Flash" `
-#         -Status "已写入 $i / $total" `
-#         -Current $i `
-#         -Total $total
-#     Start-Sleep -Milliseconds 100
-# }
+for ($i = 1; $i -le $total; $i++) {
+    Write-ProgressEx `
+    -Activity "写入 Flash" `
+    -Status "已写入 $i / $total" `
+    -Current $i `
+    -Total $total
+    Start-Sleep -Milliseconds 100
+}
 
-# Write-ProgressEx -Completed
+Write-ProgressEx -Completed
+#>
 
+<#
 function Get-AddressFromLine {
     param(
         [string]$Line
@@ -40,4 +44,4 @@ $currentAddr = $null
 }
 
 Write-ProgressEx -Completed
-
+#>
--- progress-bar.ps1
+++ progress-bar.ps1
@@ -83,23 +83,26 @@ function New-ProgressBar {
 }
 
 # ===== Demo =====
-# $progress = New-ProgressBar -Total 1000 -Activity "Flash 写入" -Id 1 -ShowETA
-# $total = 1000
-# for ($i = 1; $i -le $total; $i++) {
-#     $progress.Step(1)
-#     Start-Sleep -Milliseconds 1
-# }
+<#
+$progress = New-ProgressBar -Total 1000 -Activity "Flash 写入" -Id 1 -ShowETA
+$total = 1000
+for ($i = 1; $i -le $total; $i++) {
+    $progress.Step(1)
+    Start-Sleep -Milliseconds 1
+}
 
-# $progress.Step(128)   # 128
-# Start-Sleep -Milliseconds 200
-# $progress.Step(128)   # 256
-# Start-Sleep -Milliseconds 200
-# $progress.Set(512)    # 直接跳到 512
-# Start-Sleep -Milliseconds 200
-# $progress.Step(100)   # 612
-# Start-Sleep -Milliseconds 200
-# $progress.Complete()
+$progress.Step(128)   # 128
+Start-Sleep -Milliseconds 200
+$progress.Step(128)   # 256
+Start-Sleep -Milliseconds 200
+$progress.Set(512)    # 直接跳到 512
+Start-Sleep -Milliseconds 200
+$progress.Step(100)   # 612
+Start-Sleep -Milliseconds 200
+$progress.Complete()
+#>
 
+<#
 function Get-AddressFromLine {
     param(
         [string]$Line
@@ -111,8 +114,6 @@ function Get-AddressFromLine {
 
     return $null
 }
-
-# 假设你已知总写入大小（字节）：
 
 $currentAddr = $null
 $exe  = "Cmd_download_tool.exe"
@@ -128,3 +129,4 @@ $progress = New-ProgressBar -Total $totalBytes -Activity "Writing" -ShowETA
     $progress.Set($currentAddr)
 }
 $progress.Complete()
+#>
